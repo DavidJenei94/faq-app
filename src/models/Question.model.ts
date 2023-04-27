0a8dfa1ece38ba61d3c6e4bc4fb4bbd59caf0928
David I. Jenei <jenei.david1994@gmail.com>
2023-04-27 07:45:54 +0200

    Remove unused property from Question model

--- src/models/Question.model.ts
+++ src/models/Question.model.ts
@@ -2,7 +2,6 @@ export interface Question {
   id: number;
   questionTitle: string;
   questionDetails: string;
-  creationDate: Date | null;
   upvotes: number;
   downvotes: number;
 }
@@ -11,7 +10,6 @@ export const defaultQuestion = {
   id: 0,
   questionTitle: 'Ask a question?',
   questionDetails: 'Details about question',
-  creationDate: null,
   upvotes: 0,
   downvotes: 0,
 };
